3215134ea204c6d4c572077026997f61a35623b5
wiseman88 <wiseadam02@gmail.com>
2022-07-16 17:37:08 +0200

    BlogItemEditorPick.vue created

---
+++ resources/js/components/blog/BlogItemEditorPick.vue
@@ -0,0 +1,60 @@
+<script setup>
+
+</script>
+
+<template>
+    <div
+        class="p-4 sm:p-8 xl:py-14 md:px-10 bg-slate-200 bg-opacity-40 backdrop-filter backdrop-blur-lg shadow-lg rounded-3xl space-y-3 sm:space-y-5 !border-opacity-0 --  nc-dark-box-bg">
+                <span class="mb-2">
+                  <a class="text-xs uppercase p-2 px-3 rounded-full bg-green-200 hover:bg-green-400" href="">
+                    Programming
+                  </a>
+                </span>
+        <h3 class="text-2xl font-medium mb-2">
+            Lorem ipsum dolor sit amet consectetur.
+        </h3>
+        <div class="flex mb-2">
+                  <span class="mr-3">
+                    <img class="w-12 rounded-full" src="/img/avatar.jpg" alt="">
+                  </span>
+            <div>
+                    <span class="block mb-1 font-medium text-sm dark:text-slate-100">
+                      John J. Keeshan
+                    </span>
+                <span class="block text-xs dark:text-slate-200 text-slate-500">
+                      Sep 30, 2022
+                    </span>
+            </div>
+        </div>
+        <div class="flex space-x-2">
+            <a href=""
+               class="inline-flex items-center space-x-1.5 bg-white hover:bg-red-50 hover:text-red-500 rounded-full p-1 px-4">
+                    <span>
+                      <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24"
+                           xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round"
+                                                                    stroke-width="2"
+                                                                    d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"></path></svg>
+                    </span>
+                <span>
+                      41
+                    </span>
+            </a>
+            <a href=""
+               class="inline-flex items-center space-x-1.5 bg-white hover:bg-green-50 hover:text-green-700 rounded-full p-1 px-4">
+                    <span>
+                      <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24"
+                           xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round"
+                                                                    stroke-width="2"
+                                                                    d="M8 10h.01M12 10h.01M16 10h.01M9 16H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-5l-5 5v-5z"></path></svg>
+                    </span>
+                <span>
+                      3
+                    </span>
+            </a>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
