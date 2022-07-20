bfa129eb3f1d00c2dd2e290b39ee45eaa8e1bea8
wiseman88 <wiseadam02@gmail.com>
2022-07-20 17:15:08 +0200

    dashboard navigation refactor

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -17,37 +17,30 @@
             </div>
 
             <!-- Settings Dropdown -->
-            <div class="hidden sm:flex sm:items-center sm:ml-6">
-                <x-dropdown align="right" width="48">
-                    <x-slot name="trigger">
-                        <button
-                            class="flex items-center text-sm font-medium text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out">
-                            <div>{{ Auth::user()->name }}</div>
-
-                            <div class="ml-1">
-                                <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg"
-                                     viewBox="0 0 20 20">
-                                    <path fill-rule="evenodd"
-                                          d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                                          clip-rule="evenodd"/>
-                                </svg>
-                            </div>
-                        </button>
-                    </x-slot>
-
-                    <x-slot name="content">
-                        <!-- Authentication -->
-                        <form method="POST" action="{{ route('logout') }}">
-                            @csrf
-
-                            <x-dropdown-link :href="route('logout')"
-                                             onclick="event.preventDefault();
-                                                this.closest('form').submit();">
-                                {{ __('Log Out') }}
-                            </x-dropdown-link>
-                        </form>
-                    </x-slot>
-                </x-dropdown>
+            <div class="hidden relative sm:flex items-center">
+                <div>
+                    <button class="peer flex items-center space-x-1 text-gray-700 dark:text-gray-100">
+                        <span>{{ Auth::user()->name }}</span>
+                        <span><svg class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24"
+                                   xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round"
+                                                                            stroke-linejoin="round" stroke-width="2"
+                                                                            d="M19 9l-7 7-7-7"></path></svg></span>
+                    </button>
+                    <div
+                        class="hidden absolute peer-hover:flex hover:flex flex-col bg-white drop-shadow-lg rounded">
+                        <div>
+                            <form method="POST" action="{{ route('logout') }}">
+                                @csrf
+                                <a :href="route('logout')"
+                                   onclick="event.preventDefault();
+                                                                                this.closest('form').submit();"
+                                   class="block px-5 py-3 hover:bg-gray-200 min-w-[200px] cursor-pointer">
+                                    {{ __('Log Out') }}
+                                </a>
+                                <form/>
+                        </div>
+                    </div>
+                </div>
             </div>
 
             <!-- Hamburger -->
